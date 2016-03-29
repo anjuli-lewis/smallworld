992434defdff4a74cf97baf31434575862c79735
anjuli-lewis <anjuli.lewis@gmail.com>
2016-03-29 13:05:12 -0500

    CompLab

---
+++ src/smallworld/GraphPanel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smallworld;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author alewis19
+ */
+public class GraphPanel extends JPanel {
+    
+    public GraphPanel(Graph g) {
+        
+    }
+}
