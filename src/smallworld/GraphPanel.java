a124fe3902132f977887313658244ef1e2cc3bff
anjuli-lewis <anjuli.lewis@gmail.com>
2016-04-05 09:41:43 -0500

    Code is working

--- src/smallworld/GraphPanel.java
+++ src/smallworld/GraphPanel.java
@@ -6,6 +6,7 @@
 package smallworld;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
@@ -29,10 +30,25 @@ public class GraphPanel extends JPanel {
     private final double outerRadius;
     private TreeMap<String, LocationNode> locations;
     private ArrayList<String> lines;
-    private final String origin = "A";
-    private final String destination = "B";
-    private final Color pathColor = new Color(216, 216, 216);
+    private final String origin;
+    private final String destination;
+    private final Color pathColor = new Color(0, 13, 206);
     private final Color connectColor = new Color(25, 25, 25);
+
+    public GraphPanel(Graph g, String origin, String destination) {
+        graph = g;
+        locations = new TreeMap<>();
+        lines = new ArrayList<>(graph.V());
+        double s = g.E();
+        diameter = (s % 10) / 100;
+        System.out.println(s);
+        if (diameter == 0) {
+            diameter = 0.1;
+        }
+        outerRadius = diameter * 5;
+        this.origin = origin;
+        this.destination = destination;
+    }
 
     public GraphPanel(Graph g) {
         graph = g;
@@ -41,6 +57,8 @@ public class GraphPanel extends JPanel {
         double s = g.E();
         diameter = (s % 10) / 100;
         outerRadius = diameter * 5;
+        this.origin = null;
+        this.destination = null;
     }
 
     @Override
@@ -64,10 +82,13 @@ public class GraphPanel extends JPanel {
             g2d.fill(s);
         }
         this.generateLines();
-        PathFinder p = new PathFinder(graph, origin);
-        ArrayList<String> path = new ArrayList<>(p.distanceTo(destination));
-        for (String s : p.pathTo(destination)) {
-            path.add(s);
+        ArrayList<String> path = new ArrayList<>();
+        if (origin != null) {
+            PathFinder p = new PathFinder(graph, origin);
+            for (String s : p.pathTo(destination)) {
+                path.add(s);
+            }
+            path.trimToSize();
         }
         for (String line : lines) {
             String[] points = line.split("/");
@@ -77,13 +98,15 @@ public class GraphPanel extends JPanel {
             double y1 = parseDouble(p0[1]);
             double x2 = parseDouble(p1[0]);
             double y2 = parseDouble(p1[1]);
-            for (int i = 0; i < path.size() - 1; i++) {
-                String stop = path.get(i);
-                String nextStop = path.get(i + 1);
-                if ((locations.get(stop).atPoint(x1, y1) || locations.get(stop).atPoint(x2, y2))
-                        && (locations.get(nextStop).atPoint(x1, y1) || locations.get(nextStop).atPoint(x2, y2))) {
-                    g2d.setColor(pathColor);
-                    break;
+            if (origin != null) {
+                for (int i = 0; i < path.size() - 1; i++) {
+                    String stop = path.get(i);
+                    String nextStop = path.get(i + 1);
+                    if ((locations.get(stop).atPoint(x1, y1) || locations.get(stop).atPoint(x2, y2))
+                            && (locations.get(nextStop).atPoint(x1, y1) || locations.get(nextStop).atPoint(x2, y2))) {
+                        g2d.setColor(pathColor);
+                        break;
+                    }
                 }
             }
             Shape e = new Line2D.Double(x1, y1, x2, y2);
@@ -135,11 +158,17 @@ public class GraphPanel extends JPanel {
                         y = loc.getY() + outerRadius * Math.sin(angle);
                         angle += angleIncrease;
                         n = new LocationNode(x, y);
-                    } while ((x > 1 - diameter * 2) || (x < -1) || (y > 1 - diameter * 2) || (y < -1) || locations.containsValue(n));
+                    } while ((x < 1-diameter/2) && (x > -1) && (y > 1 - diameter/2) && (y < -1) && !locations.containsValue(n));
                     locations.put(node, n);
-                    if (graph.degree(node)>1) {
-                        this.circleWithCenter(node);
-                        break;
+                }
+            }
+            for (String node : nodes) {
+                if (graph.degree(node) > 1) {
+                    for (String s : graph.adjacentTo(node)) {
+                        if (!locations.containsKey(s)) {
+                            this.circleWithCenter(node);
+                            break;
+                        }
                     }
                 }
             }
@@ -156,16 +185,23 @@ public class GraphPanel extends JPanel {
         for (String node : nodes) {
             if (!locations.containsKey(node)) {
                 do {
-                    x = loc.getX() + outerRadius * Math.cos(angle);
-                    y = loc.getY() + outerRadius * Math.sin(angle);
+                    x = outerRadius * Math.cos(angle);
+                    y = outerRadius * Math.sin(angle);
                     angle += angleIncrease;
                     n = new LocationNode(x, y);
-                } while ((x > 1 - diameter) || (x < -1) || (y > 1 - diameter) || (y < -1) || locations.containsValue(n));
+                } while ((x > 1 - diameter) && (x < -1) && (y > 1 - diameter) && (y < -1) && !locations.containsValue(n));
                 locations.put(node, n);
             }
         }
         for (String node : nodes) {
-            circleWithCenter(node);
+            if (graph.degree(node) > 1) {
+                for (String s : graph.adjacentTo(node)) {
+                    if (!locations.containsKey(s)) {
+                        this.circleWithCenter(node);
+                        break;
+                    }
+                }
+            }
         }
     }
 
