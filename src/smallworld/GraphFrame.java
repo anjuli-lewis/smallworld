03529f8d7dd60e40979241f5bb6b0f1ec42abf76
anjuli-lewis <anjuli.lewis@gmail.com>
2016-04-05 09:41:56 -0500

    Code is working

--- src/smallworld/GraphFrame.java
+++ src/smallworld/GraphFrame.java
@@ -5,6 +5,7 @@
  */
 package smallworld;
 
+import edu.princeton.cs.In;
 import java.awt.Container;
 import javax.swing.JFrame;
 
@@ -16,24 +17,32 @@ public class GraphFrame extends JFrame {
 
     public static final int FRAME_WIDTH = 768;
     public static final int FRAME_HEIGHT = 768;
-    
+
     public GraphFrame(Graph g) {
         this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container pane = this.getContentPane();
-        GraphPanel panel = new GraphPanel(g);
+        GraphPanel panel = new GraphPanel(g,"A","H");
         pane.add(panel);
         this.setVisible(true);
 
     }
+
     public static void main(String[] args) {
         Graph G = new Graph();
-        //two centers
+//        two centers
 //        G.addEdge("A", "B");
 //        G.addEdge("A", "C");
-//        G.addEdge("F", "D");
-//        G.addEdge("F", "E");
-        //one center
+//        G.addEdge("F", "B");
+//        G.addEdge("F", "C");
+//        G.addEdge("K", "A");
+//        G.addEdge("F", "Z");
+//        G.addEdge("M", "A");
+//        G.addEdge("F", "T");
+//        G.addEdge("U", "A");
+//        G.addEdge("F", "V");
+//        G.addEdge("Y", "V");
+//        one center
         G.addEdge("A", "B");
         G.addEdge("A", "C");
         G.addEdge("A", "D");
@@ -41,13 +50,25 @@ public class GraphFrame extends JFrame {
         G.addEdge("A", "F");
         G.addEdge("A", "G");
         G.addEdge("B", "H");
+        G.addEdge("B","C");
+        G.addEdge("B","D");
+        G.addEdge("D","F");
+        G.addEdge("D","Z");
+        G.addEdge("D","K");
+        G.addEdge("Z","K");
+        G.addEdge("K","F");
+        G.addEdge("G","L");
         //no center
 //        G.addEdge("A", "B");
+//        G.addEdge("A", "C");
+//        G.addEdge("A", "D");
+//        G.addEdge("A", "E");
 //        G.addEdge("B", "C");
+//        G.addEdge("B", "D");
+//        G.addEdge("B", "E");
 //        G.addEdge("C", "D");
+//        G.addEdge("C", "E");
 //        G.addEdge("D", "E");
-//        G.addEdge("E", "F");
-//        G.addEdge("F", "G");
-        GraphFrame g=new GraphFrame(G);
+        GraphFrame g = new GraphFrame(G);
     }
 }
